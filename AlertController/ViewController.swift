ff25985be77c9067394ab1fa84f8c3d83a40c41b
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-04-19 11:40:55 +0900

    AlertController

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let myAlert = UIAlertController(title: "알림", message: "설정된 시간이 되었습니다!!", preferredStyle: .alert)
+        
+        //세번째 인자를 클로져 사용
+        //후행 클로져(Trailing Closure)
+        let okAction = UIAlertAction(title: "종료", style: .default, handler: {(action: UIAlertAction) -> Void in
+            self.view.backgroundColor = UIColor.yellow
+        })
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: { (action: UIAlertAction) -> Void in self.view.backgroundColor = UIColor.white})
+        
+        
+        myAlert.addAction(okAction)
+        myAlert.addAction(cancelAction)
+        
+        present(myAlert, animated: true, completion: nil)
+    }
+    
 }
+
 
